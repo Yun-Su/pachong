c6799d3344c610ef4ee23b8dcd833d74277f65a0
YunSu <Stack_Overflow_Error@outlook.com>
2019-07-05 22:13:32 +0800

    .

--- pachong/PorxyHandler代理.py
+++ pachong/PorxyHandler代理.py
@@ -1,7 +1,7 @@
 from urllib import request
 from urllib.request import ProxyHandler, build_opener
 url='http://httpbin.org/ip'
-proxy_handler=ProxyHandler({'http':'http://61.164.39.67:53281'})
-opener=build_opener(proxy_handler)
+proxy_handler=request.ProxyHandler({'http':'http://61.164.39.67:53281'})
+opener=request.build_opener(proxy_handler)
 resp=opener.open(url)
 print(resp.read())
